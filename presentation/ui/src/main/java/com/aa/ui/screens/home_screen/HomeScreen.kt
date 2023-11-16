afef860a43e818b7f6fb8d39fb649f0da7938c70
AndrewAboalhana <hana_hany6@yahoo.com>
2023-11-16 18:58:27 +0200

    finish navigation bar

--- presentation/ui/src/main/java/com/aa/ui/screens/home_screen/HomeScreen.kt
+++ presentation/ui/src/main/java/com/aa/ui/screens/home_screen/HomeScreen.kt
@@ -19,10 +19,15 @@ import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -32,6 +37,7 @@ import com.aa.ui.screens.home_screen.composables.BabyInfoCard
 import com.aa.ui.screens.home_screen.composables.CalendarItem
 import com.aa.ui.screens.home_screen.composables.HomeHeader
 import com.aa.ui.screens.home_screen.composables.PregnancyProgressBar
+import com.aa.ui.screens.navigation_bar.NavItem
 import com.aa.ui.screens.navigation_bar.NavigationBar
 import com.aa.viewmodels.home_screen.HomeScreenViewModel
 import com.aa.viewmodels.home_screen.HomeUiState
@@ -60,73 +66,88 @@ private fun HomeContent(
     onCalendarItemSelected: (Int) -> Unit,
     onDetailsCard: (Int) -> Unit
 ) {
-    Column {
-        HomeHeader()
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-                .padding(12.dp)
-        ) {
-            item {
-                Box {
-                    Column {
-                        LazyRow(
-                            horizontalArrangement = Arrangement.spacedBy(8.dp),
-                            contentPadding = PaddingValues(vertical = 12.dp)
-                        ) {
-                            items(state.calender) { index ->
-                                val isClicked = index == selectedCalendarItem
-                                CalendarItem(number = index, isClicked = isClicked) {
-                                    onCalendarItemSelected(it-1)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column( modifier = Modifier.padding(bottom = 48.dp)) {
+
+            HomeHeader()
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .padding(12.dp)
+            ) {
+                item {
+                    Box {
+                        Column {
+                            LazyRow(
+                                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                                contentPadding = PaddingValues(vertical = 12.dp)
+                            ) {
+                                items(state.calender) { index ->
+                                    val isClicked = index == selectedCalendarItem
+                                    CalendarItem(number = index, isClicked = isClicked) {
+                                        onCalendarItemSelected(it - 1)
+                                    }
                                 }
                             }
-                        }
-                        Card(
-                            modifier = Modifier
-                                .size(344.dp, 368.dp)
-                                .align(Alignment.CenterHorizontally)
-                                .clickable { onDetailsCard(state.weekId - 1) },
-                        ) {
-                            Image(
-                                painter = rememberAsyncImagePainter(model = state.babyImage),
-                                contentDescription = "babyImage",
-                                contentScale = ContentScale.FillBounds,
-                                modifier = Modifier.fillMaxSize(),
+                            Card(
+                                modifier = Modifier
+                                    .size(344.dp, 368.dp)
+                                    .align(Alignment.CenterHorizontally)
+                                    .clickable { onDetailsCard(state.weekId - 1) },
+                            ) {
+                                Image(
+                                    painter = rememberAsyncImagePainter(model = state.babyImage),
+                                    contentDescription = "babyImage",
+                                    contentScale = ContentScale.FillBounds,
+                                    modifier = Modifier.fillMaxSize(),
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.height(12.dp))
+
+                            BabyInfoCard(
+                                state = state,
+                                modifier = Modifier.align(Alignment.CenterHorizontally),
+                                onButtonClicked = { onDetailsCard(state.weekId - 1) }
                             )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            PregnancyProgressBar()
+
+                            Spacer(modifier = Modifier.height(24.dp))
+
                         }
-
-                        Spacer(modifier = Modifier.height(12.dp))
-
-                        BabyInfoCard(
-                            state = state,
-                            modifier = Modifier.align(Alignment.CenterHorizontally),
-                            onButtonClicked = { onDetailsCard(state.weekId-1) }
-                        )
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        PregnancyProgressBar()
-
                     }
                 }
             }
         }
-        NavigationBar(navController = navController , modifier = Modifier.weight(1f))
+        NavigationBar(
+            navController = navController,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(12.dp),
+            selectedIcon = NavItem.Home
+        )
     }
 }
 
 
-//@Preview
-//@Composable
-//fun HomeContentPreview() {
-//    var selectedCalendarItem by remember { mutableIntStateOf(1) }
-//
-//    HomeContent(
-//        state = HomeUiState(calender = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
-//        selectedCalendarItem = selectedCalendarItem,
-//        onCalendarItemSelected = { selectedCalendarItem = it },
-//    ) {
-//        selectedCalendarItem = it
-//    }
-//}
+@Preview
+@Composable
+fun HomeContentPreview() {
+    var selectedCalendarItem by remember { mutableIntStateOf(1) }
+
+    HomeContent(
+        state = HomeUiState(calender = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
+        selectedCalendarItem = selectedCalendarItem,
+        onCalendarItemSelected = { selectedCalendarItem = it },
+        navController = NavController(LocalContext.current)
+    ) {
+        selectedCalendarItem = it
+    }
+}
--- presentation/ui/src/main/java/com/aa/ui/screens/navigation_bar/NavigationBar.kt
+++ presentation/ui/src/main/java/com/aa/ui/screens/navigation_bar/NavigationBar.kt
@@ -3,19 +3,16 @@ package com.aa.ui.screens.navigation_bar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,15 +30,23 @@ import com.aa.ui.screens.home_screen.navigateToHomeScreen
 @Composable
 fun NavigationBar(
     navController: NavController,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    selectedIcon: NavItem = NavItem.Home
 ) {
     NavigationBarContent(
-        onHomeClick = navController::navigateToHomeScreen,
-        onDiscoverClick = navController::navigateToDiscoverScreen,
-        modifier = modifier
+        onHomeClick = {
+            navController.navigateToHomeScreen()
+        },
+        onDiscoverClick = {
+            navController.navigateToDiscoverScreen()
+        },
+        onChatBotClick = {},
+        onProfileClick = {},
+        modifier = modifier,
+        selectedIcon = selectedIcon
     )
-    
 }
+
 @Composable
 private fun NavigationBarContent(
     modifier: Modifier = Modifier,
@@ -49,18 +54,17 @@ private fun NavigationBarContent(
     onDiscoverClick: () -> Unit = {},
     onChatBotClick: () -> Unit = {},
     onProfileClick: () -> Unit = {},
-    isClicked: Boolean = false,
-    ) {
-
-    val iconColor = if (isClicked) Color(0xFFF15223) else Color(0xffeaecf0)
-
+    selectedIcon : NavItem = NavItem.Home
+) {
+    // State to keep track of the selected icon
+    var item by remember { mutableStateOf(selectedIcon) }
 
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth()
-            .height(64.dp)
+            .height(58.dp)
             .border(
                 width = 0.5.dp,
                 color = Color(0xFFCDCDD0),
@@ -69,28 +73,30 @@ private fun NavigationBarContent(
             .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 64.dp))
             .padding(horizontal = 24.dp)
     ) {
-        Icon(
-            painter = painterResource(id = R.drawable.home_button),
+        NavigationBarItem(
+            iconRes = R.drawable.home_button,
             contentDescription = "home",
-            tint = iconColor,
-            modifier = Modifier
-                .size(28.dp)
-                .clickable { onHomeClick() }
+            onClick = {
+                onHomeClick()
+                item = NavItem.Home
+            },
+            isSelected = item == NavItem.Home
         )
         Spacer(modifier = Modifier.width(40.dp))
-        Icon(
-            painter = painterResource(id = R.drawable.discover_button),
-            contentDescription = "home",
-            tint = iconColor,
-            modifier = Modifier
-                .size(28.dp)
-                .clickable { onDiscoverClick()}
+        NavigationBarItem(
+            iconRes = R.drawable.discover24,
+            contentDescription = "discover",
+            onClick = {
+                onDiscoverClick()
+                item = NavItem.Discover
+            },
+            isSelected = item == NavItem.Discover
         )
         Spacer(modifier = Modifier.width(36.dp))
         Box(
             modifier = Modifier
                 .size(48.dp)
-                .clickable { }
+                .clickable { /* Handle click if needed */ }
                 .clip(shape = CircleShape)
                 .background(Brush.linearGradient(listOf(Color(0xFFF15223), Color(0xFFE3524F))))
                 .shadow(
@@ -101,31 +107,60 @@ private fun NavigationBarContent(
         )
         Spacer(modifier = Modifier.width(36.dp))
 
-        Icon(
-            painter = painterResource(id = R.drawable.message_bot),
-            contentDescription = "home",
-            tint = iconColor,
-            modifier = Modifier
-                .size(28.dp)
-                .clickable { }
+        NavigationBarItem(
+            iconRes = R.drawable.message_bot,
+            contentDescription = "chat bot",
+            onClick = {
+                onChatBotClick()
+                item = NavItem.ChatBot
+            },
+            isSelected = item == NavItem.ChatBot
         )
 
         Spacer(modifier = Modifier.width(40.dp))
 
-        Icon(
-            painter = painterResource(id = R.drawable.profile_button),
-            contentDescription = "home",
-            tint = iconColor,
-            modifier = Modifier
-                .size(28.dp)
-                .clickable { }
+        NavigationBarItem(
+            iconRes = R.drawable.profile_button,
+            contentDescription = "profile",
+            onClick = {
+                onProfileClick()
+                item = NavItem.Profile
+            },
+            isSelected = item == NavItem.Profile
         )
-
     }
+}
+
+@Composable
+private fun NavigationBarItem(
+    iconRes: Int,
+    contentDescription: String,
+    onClick: () -> Unit,
+    isSelected: Boolean
+) {
+    Surface(
+        modifier = Modifier
+            .size(28.dp)
+            .clip(CircleShape)
+            .clickable { onClick() }
+    ) {
+        Icon(
+            painter = painterResource(id = iconRes),
+            contentDescription = contentDescription,
+            tint = if (isSelected) Color(0xFFF15223) else Color.LightGray
+        )
+    }
+}
+
+enum class NavItem {
+    Home,
+    Discover,
+    ChatBot,
+    Profile
 }
 
 @Preview
 @Composable
 fun NavigationBarPreview() {
-    NavigationBarContent(isClicked = false)
+    NavigationBarContent()
 }
