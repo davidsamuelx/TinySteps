18367e1a04148384423a9d136fa8b1cfff462d79
antonnagi24@gmail.com <93437848+Antonnagi@users.noreply.github.com>
2023-12-04 22:05:37 +0200

    shimmer

--- presentation/ui/src/main/java/com/aa/ui/screens/phase_1/search/composable/ItemCard.kt
+++ presentation/ui/src/main/java/com/aa/ui/screens/phase_1/search/composable/ItemCard.kt
@@ -12,10 +12,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.aa.ui.R
+import com.aa.ui.screens.phase_1.composable.ShimmerDiscover
 
 @RequiresApi(Build.VERSION_CODES.Q)
 @Composable
@@ -38,56 +41,70 @@ fun ItemCard(
     onClickItem: (Int) -> Unit,
     title: String,
     imageUrl: String,
+    isLoading:Boolean
 ){
-    Card(
-        modifier = modifier
-            .height(180.dp)
-            .clip(shape = RoundedCornerShape(size = 20.dp))
-            .border(
-                width = 1.dp,
-                color = Color(0xFFEAECF0),
-                shape = RoundedCornerShape(size = 20.dp)
-            )
-            .clickable(onClick = { onClickItem(id) })
-    ){
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFFEAECF0)),
-            contentAlignment = Alignment.BottomStart
-        ) {
-            Image(
-                painter = rememberAsyncImagePainter(
-                    model=imageUrl,
-                    placeholder = painterResource(id = R.drawable.placeholde_image)
-                    ) ,
-                contentDescription = null,
-                contentScale = ContentScale.FillBounds,
-                modifier = Modifier
-                    .fillMaxSize()
-                )
-            Text(
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .fillMaxWidth()
-                    .background(
-                        Brush.verticalGradient(
-                            0F to Color.Transparent,
-                            .5F to Color.Black.copy(alpha = 0.5F),
-                            1F to Color.Black.copy(alpha = 0.8F)
+//    var  isLoadings by remember{
+//        mutableStateOf(true)
+//    }
+    LaunchedEffect(key1 = false ){
+
+        !isLoading
+    }
+    ShimmerDiscover(isLoading =isLoading,
+        contextAfterLoading ={
+            Card(
+                modifier = modifier
+                    .height(180.dp)
+                    .wrapContentSize()
+                    .clip(shape = RoundedCornerShape(size = 20.dp))
+                    .border(
+                        width = 1.dp,
+                        color = Color(0xFFEAECF0),
+                        shape = RoundedCornerShape(size = 20.dp)
+                    )
+                    .clickable(onClick = { onClickItem(id) })
+            ){
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFEAECF0)),
+                    contentAlignment = Alignment.BottomStart
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(
+                            model=imageUrl,
+                            placeholder = painterResource(id = R.drawable.placeholde_image)
+                        ) ,
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+                    Text(
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                            .fillMaxWidth()
+                            .background(
+                                Brush.verticalGradient(
+                                    0F to Color.Transparent,
+                                    .5F to Color.Black.copy(alpha = 0.5F),
+                                    1F to Color.Black.copy(alpha = 0.8F)
+                                )
+                            )
+                            .padding(bottom = 16.dp, start = 22.dp),
+                        text = title,
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            lineHeight = 32.sp,
+                            fontWeight = FontWeight(700),
+                            color = Color(0xFFFFFFFF),
                         )
                     )
-                    .padding(bottom = 16.dp, start = 22.dp),
-                text = title,
-                style = TextStyle(
-                    fontSize = 24.sp,
-                    lineHeight = 32.sp,
-                    fontWeight = FontWeight(700),
-                    color = Color(0xFFFFFFFF),
-                )
-            )
+                }
+            }
         }
-    }
+    )
+
 }
 
 //@RequiresApi(Build.VERSION_CODES.Q)
