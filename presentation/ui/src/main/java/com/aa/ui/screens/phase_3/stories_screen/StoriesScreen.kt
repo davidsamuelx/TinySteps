6d4a043938e81547cb109f7e4f386b3cf1f48f1d
David samuel <davidsamuelramsis@gmail.com>
2023-12-22 19:59:23 +0200

    remove unused imports

--- presentation/ui/src/main/java/com/aa/ui/screens/phase_3/stories_screen/StoriesScreen.kt
+++ presentation/ui/src/main/java/com/aa/ui/screens/phase_3/stories_screen/StoriesScreen.kt
@@ -10,16 +10,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.aa.ui.screens.phase_1.home_screen.composables.HomeHeader
